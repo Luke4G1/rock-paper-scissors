56dad6409ea90090a0b3e2e5d6379f19f811d5cd
Luke4G1 <theooldlyon@gmail.com>
2022-07-05 16:51:12 +0200

    created the playRound function and made it parameter case insenitive

--- main.js
+++ main.js
@@ -8,3 +8,45 @@ function computerPlay() {
 }
 
 
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerSelection.toLowerCase()
+
+    var result;
+
+    switch (playerSelection) {
+        case 'rock':
+            if (computerSelection == 'rock') {
+                result = `Draw`
+            } else if (computerSelection == 'paper') {
+                result = `You lose, ${computerSelection} beats ${playerSelection}`
+            } else if (computerSelection == 'scissors') {
+                result = `You win, ${playerSelection} beats ${computerSelection}`
+            }
+            break;
+        case 'paper':
+            if (computerSelection == 'paper') {
+                result = `Draw`
+            } else if (computerSelection == 'scissors') {
+                result = `You lose, ${computerSelection} beats ${playerSelection}`
+            } else if (computerSelection == 'rock') {
+                result = `You win, ${playerSelection} beats ${computerSelection}`
+            }
+            break;
+        case 'scissors':
+            if (computerSelection == 'scissors') {
+                result = `Draw`
+            } else if (computerSelection == 'rock') {
+                result = `You lose, ${computerSelection} beats ${playerSelection}`
+            } else if (computerSelection == 'paper') {
+                result = `You win, ${playerSelection} beats ${computerSelection}`
+            }
+            break;
+
+        default:
+            console.log('Choose a valid input')
+    }
+
+    return result
+}
